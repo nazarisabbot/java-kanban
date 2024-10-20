a0f5d210173a9321ae9dfab5d453eb3099e4cb71
nazar <n.updesign@gmail.com>
2024-10-20 14:56:27 +0500

    Реализовать автосохрание состояния задач в файл, востановление задач из файла r2

--- src/tracker/controllers/ManagerSaveException.java
+++ src/tracker/controllers/ManagerSaveException.java
@@ -1,7 +1,5 @@
 package tracker.controllers;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final String message) {
         super(message);
